249a4523118b0a1f21e52aeeba8d1eebdc7ffe35
SiverDX <kai.zahn@yahoo.de>
2021-10-06 20:23:29 +0200

    Get Token with client credentials for normal API call + also fixed url (de.api* -> eu.api)

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,35 +4,30 @@ export default class Characters extends React.Component {
     constructor(props) {
         super(props);
 
-        let apiURL = 'https://de.api.blizzard.com/profile/wow/character/Thrall/Nadare/collections';
-        let tokenURL = 'https://eu.battle.net/oauth/authorize';
+        let apiURL = 'https://eu.api.blizzard.com/profile/wow/character/Thrall/Nadare/collections';
+        let tokenURL = 'https://eu.battle.net/oauth/token';
 
-        fetch(tokenURL, {
-            method: 'GET',
-            mode: 'no-cors',
-            headers: {
-                'region': 'eu',
-                'reponse_type': 'code',
-                'client_id': process.env.REACT_APP_CLIENT_ID,
-                'redirect_uri': 'http://localost:3000',
-                'scope': 'wow',
-                'state': 'test'
-            }
-        }).then(response => response.json()).then(data => {
+        let authorization = btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET);
+
+        let formData = new FormData();
+        formData.append('grant_type', 'client_credentials');
+
+        let options = {method: 'POST', headers: {'Authorization': 'Basic ' + authorization}, body: formData};
+
+        fetch(tokenURL, options).then(response => response.json()).then(data => {
             console.log(data)
-        }).catch(reason => console.error(reason));
 
-        fetch(apiURL, {
-            method: 'GET',
-            mode: 'no-cors',
-            headers: {
-                'namespace': 'profile-eu',
-                'locale': 'de_de',
-                'access_token': process.env.REACT_APP_CLIENT_SECRET
-            }
-        }).then(response => {console.log(response); return response.json();}).then(data => {
-            console.log(data);
-            this.state = {mounts: {apiURL: data.mounts.href}};
+            fetch(apiURL, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + data.access_token,
+                    'namespace': 'profile-eu',
+                    'locale': 'de_de',
+                }
+            }).then(response => {console.log(response); return response.json();}).then(data => {
+                console.log(data);
+                this.state = {mounts: {apiURL: data.mounts.href}};
+            }).catch(reason => console.error(reason));
         }).catch(reason => console.error(reason));
     }
 
