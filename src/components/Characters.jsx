a0c960c58cd898dc12e149bb2e31c5a8a9dac215
SiverDX <kai.zahn@yahoo.de>
2021-10-06 21:03:47 +0200

    Functional API call with UI result

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,7 +4,30 @@ export default class Characters extends React.Component {
     constructor(props) {
         super(props);
 
-        let apiURL = 'https://eu.api.blizzard.com/profile/wow/character/Thrall/Nadare/collections';
+        this.state = {mounts: []};
+    }
+
+    render() {
+        return (
+            <div>
+                <input id='realmInput' placeholder='Realm'/>
+                <input id='characterNameInput' placeholder='Character Name'/>
+                <button onClick={this.fetchMounts}>Get Mounts</button>
+                {/* only render when the api has loaded the mounts */}
+                {this.state.mounts.length > 0 &&
+                <ul>
+                    {this.state.mounts.map(function (mount, index) {
+                        return <li key={index}>{mount.mount.name}</li>;
+                    })}
+                </ul>
+                }
+                {/*<Mounts apiURL={this.state.mounts.apiURL}/>*/}
+            </div>
+        )
+    }
+
+    /* todo :: keep in mind -> arrow function required to bind 'this' */
+    fetchMounts = () => {
         let tokenURL = 'https://eu.battle.net/oauth/token';
 
         let authorization = btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET);
@@ -14,29 +37,26 @@ export default class Characters extends React.Component {
 
         let options = {method: 'POST', headers: {'Authorization': 'Basic ' + authorization}, body: formData};
 
+        let realm = document.getElementById('realmInput').value.toLowerCase();
+        let character = document.getElementById('characterNameInput').value.toLowerCase();
+        let apiURL = 'https://eu.api.blizzard.com/profile/wow/character/' + realm + "/" + character + "/collections/mounts?namespace=profile-eu&locale=de_de";
+
         fetch(tokenURL, options).then(response => response.json()).then(data => {
             console.log(data)
 
             fetch(apiURL, {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer ' + data.access_token,
-                    'namespace': 'profile-eu',
-                    'locale': 'de_de',
+                    'Authorization': 'Bearer ' + data.access_token
                 }
-            }).then(response => {console.log(response); return response.json();}).then(data => {
+            }).then(response => response.json()).then(data => {
                 console.log(data);
-                this.state = {mounts: {apiURL: data.mounts.href}};
+
+                /* code only gets set if an error occurs */
+                if (!data.code) {
+                    this.setState({mounts: data.mounts});
+                }
             }).catch(reason => console.error(reason));
         }).catch(reason => console.error(reason));
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello there, {this.props.name} </h1>
-                {/*<Mounts apiURL={this.state.mounts.apiURL}/>*/}
-            </div>
-        )
     }
 }
