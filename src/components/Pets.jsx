f864f28f31e7371815a47f60eff6d468e57ccafc
SiverDX <kai.zahn@yahoo.de>
2021-10-08 23:54:52 +0200

    added LazyLoad library and removed 'eject' command

--- src/components/Pets.jsx
+++ src/components/Pets.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import LazyLoad from 'react-lazyload';
 
 export default class Pets extends React.Component {
+    /* todo ::
+     on mouseover call wowhead api
+    */
     render() {
         return (
-            <ol>
-                {this.props.data.pets.map(function (item, index) {
-                    return <li key={index}>{item.species.name}</li>;
-                })}
-            </ol>
+            <div>
+                <h1>Pets ({this.props.data.pets.length})</h1>
+                <div className='image-container'>
+                    {this.props.data.pets.map(function (item, index) {
+                        let imageSource;
+
+                        if (item.creature_display) {
+                            /* some pets are missing this */
+                            imageSource = `https://render-eu.worldofwarcraft.com/npcs/zoom/creature-display-${item.creature_display.id}.jpg`;
+                        } else {
+                            imageSource = 'https://dummyimage.com/600x600/000/fff';
+                        }
+
+                        return (
+                            <LazyLoad height={600} key={index}>
+                                <img src={imageSource} alt={item.species.name} key={index} title={item.species.name}/>
+                            </LazyLoad>
+                        );
+                    })}
+                </div>
+            </div>
         )
     }
 }
